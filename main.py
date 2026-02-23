c341ff0e7949d98a459e802073c8dfc4dd98412e
1mWan <123259182+Easygmou@users.noreply.github.com>
2026-02-23 22:56:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("Fantasy-Bot", "1mWan", "一个简单的 Fantasy-Bot 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -10,15 +10,26 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    # 注册指令的装饰器。指令名为 verify。注册成功后，发送 `/verify` 就会触发这个指令，并post请求body为发送人qq号访问https://127.0.0.1:10001/bot/verify
+    @filter.command("verify")
+    async def verify(self, event: AstrMessageEvent):
+        """这是一个 verify 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        # 判断qq群聊是否为1084990390
+        if event.get_group_id() != 1084990390:
+            return
+        user_id = event.get_sender_id()
+        logger.info(f"verify {user_id}")
+        # 发送post请求
+        import requests
+        url = "https://127.0.0.1:10001/bot/verify"
+        data = {"qq": user_id}
+        response = requests.post(url, json=data)
+        logger.info(response.text)
+        # 解析json
+        import json
+        data = json.loads(response.text)
+        # 回复消息
+        yield event.plain_result(data["message"])
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
